0e56e7d937559ca6f72631c035c7760c8c9a3c91
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-04-07 12:25:26 -0600

    Update QRFactorization.py

--- QRFactorization.py
+++ QRFactorization.py
@@ -14,17 +14,17 @@ import pprint
 import scipy
 import scipy.linalg   # SciPy Linear Algebra Library
 
-   A = scipy.array([[1, -4], [2,3], [2,2]])  # test values, also tested 3 by 3
-   Q, R = scipy.linalg.qr(A)
-   
-   print "A:"
-   pprint.pprint(A)
-   
-   print "Q:"
-   pprint.pprint(Q)
-   
-   print "R:"          # R must be an upper triangular matrix
-   pprint.pprint(R)
+A = scipy.array([[1, -4], [2,3], [2,2]])  # test values, also tested 3 by 3
+Q, R = scipy.linalg.qr(A)
+
+print "A:"
+pprint.pprint(A)
+
+print "Q:"
+pprint.pprint(Q)
+
+print "R:"          # R must be an upper triangular matrix
+pprint.pprint(R)
 
    
    
