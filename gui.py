6f80cea699127ae13c7bd6b6f28e149254db74cb
justinshapiro <justin.shapiro@ucdenver.edu>
2017-04-22 23:34:30 -0600

    Fixed "Bad label anchor specification"

--- gui.py
+++ gui.py
@@ -67,7 +67,7 @@ class App(Frame):
 
     def createWidgets(self):
         # Interpolation Frame
-        self.interpLabelframe = LabelFrame(self.root, text = "Interpolation", labelanchor=N)
+        self.interpLabelframe = LabelFrame(self.root, text = "Interpolation", labelanchor='n')
         self.interpLabelframe.grid(row = 0, columnspan = 7, sticky = 'WE', padx = 5, pady = 5, ipadx = 5, ipady = 5)
         self.interpLabelframe.pack(fill = 'both', expand = 'yes')
         self.innerInFrame = Frame(self.interpLabelframe)
@@ -86,7 +86,7 @@ class App(Frame):
         self.bezierButton.grid(row=1, column=1, columnspan=2)
 
         # Least Squares Frame
-        self.lsLabelframe = LabelFrame(self.root, text = "Least Squares", labelanchor = N)
+        self.lsLabelframe = LabelFrame(self.root, text = "Least Squares", labelanchor='n')
         self.lsLabelframe.grid(row = 2, sticky = 'WE', \
                                padx = 5, pady = 5, ipadx = 5, ipady = 5)
         self.lsLabelframe.pack(fill = 'both', expand = 'yes')
@@ -100,13 +100,13 @@ class App(Frame):
         self.nonLinBtn.grid(row = 0, column = 1)
 
         # Differentiation and Integration Frame
-        self.diffAndInt = Frame(self.root)#, text = "Differentiation and Integration", labelanchor = N)
+        self.diffAndInt = Frame(self.root)
         self.diffAndInt.grid(row = 0, columnspan = 7, sticky = 'WE', padx = 5, pady = 5, ipadx = 10, ipady = 5)
         self.diffAndInt.pack(fill = 'both', expand = 'yes')
 
         # Sub Frames
-        self.diffFrame = LabelFrame(self.diffAndInt, text = "Differentiation", labelanchor = N)
-        self.intFrame = LabelFrame(self.diffAndInt, text = "Integration", labelanchor = N)
+        self.diffFrame = LabelFrame(self.diffAndInt, text = "Differentiation", labelanchor='n')
+        self.intFrame = LabelFrame(self.diffAndInt, text = "Integration", labelanchor='n')
         self.diffFrame.grid(row = 0, column = 0, columnspan = 2)
         self.intFrame.grid(row = 1, column = 0)
         self.diffFrame.pack(expand = 'yes')
