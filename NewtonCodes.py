3c85a5351bfb1c5492aa39be24f4dba71bb58ef4
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-04-23 21:05:37 -0600

    added Romberg Method, sign and error need to be fixed

--- NewtonCodes.py
+++ NewtonCodes.py
@@ -55,16 +55,43 @@ def simpson(f, a, b, n):
     simp = (h / 3.0) * simp
     return simp
 
-
+# romberg method
+ # -------------------------------------
+ def romberg(f,a,b,n):
+    # initialize romberg matrix
+    steps = []
+    R      = np.zeros((n,n))
+    R[0,0] = 0.5*(b-a)*(f(a)+f(b))
+    # fill first column of romberg matrix
+    for i in range(1,n):
+        pts  = 2**i
+        h    = (b-a)/float(pts)
+        steps.append(h)
+        psum = 0
+        for j in range(1, pts):
+            if j % 2 != 0:
+                psum += f(a + j*h)
+        # populate matrix
+        R[i,0] = 0.5*R[i-1,0] + h*psum
+    # get integral from romberg matrix
+    for i in range(1,n):
+        for j in range(i,n):
+            r1 = R[j,i-1]
+            r0 = R[j-1,i-1]
+            k  = 1.0/(4.0**i-1)
+            R[j,i] = r1 + k*(r1 - r0)
+    romb = R[n-1,n-1]
+    return romb
+#-------------------------------------
 
 # call integration routines
 trap = trapezoid(f,a,b,n)
 simp = simpson(f,a,b,n)
-
+romb = romberg(f,a,b,n)
 # get integration error
 terr = (abs(asol-trap)/asol)*100
 serr = (abs(asol-simp)/asol)*100
-
+rerr = (abs(asol-romb)/asol)*100
 # print results
 
 print '\nMethod            Solution    Error'
@@ -72,3 +99,4 @@ print '--------------------------------------'
 print 'analytical     %12.6f   %6.3f'  % (asol,0),'%'
 print 'trapezoid      %12.6f   %6.3f'  % (trap,terr),'%'
 print 'simpson        %12.6f   %6.3f'  % (simp,serr),'%'
+print 'romberg        %12.6f   %6.3f'  % (romb,rerr),'%
